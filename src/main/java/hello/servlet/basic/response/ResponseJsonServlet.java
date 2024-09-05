8cca9c890491c2c4f39e46c76d227c4ee71aefbc
jarajiri <eic3021@gmail.com>
2024-09-05 21:59:20 +0900

    [서블릿, JSP, MVC] - MVC 패턴 개요까지 완료

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,13 +14,13 @@ import java.io.IOException;
 @WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Content-Type: application/json
         response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
+        // response.setCharacterEncoding("utf-8");
 
         HelloData helloData = new HelloData();
         helloData.setUsername("kim");
@@ -27,6 +28,14 @@ public class ResponseJsonServlet extends HttpServlet {
 
         //{"username":"kim", "age":20}
         String result = objectMapper.writeValueAsString(helloData);
+        // 출력 스트림에 JSON 문자열을 출력
+
+        // getWriter() - 추가 파라미터 자동추가 ex) charset=" "
         response.getWriter().write(result);
+
+        // OutputStream()
+        ServletOutputStream outputStream = response.getOutputStream();
+        outputStream.write(result.getBytes());
+        outputStream.flush();  // 스트림을 비우고 강제로 출력
     }
 }
